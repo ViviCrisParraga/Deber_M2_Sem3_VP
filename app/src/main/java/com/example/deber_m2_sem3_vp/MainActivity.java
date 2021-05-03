46064b48b66414906303f51c8c5c786725435d34
ViviCrisParraga <83534501+ViviCrisParraga@users.noreply.github.com>
2021-05-03 00:22:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/deber_m2_sem3_vp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.deber_m2_sem3_vp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
